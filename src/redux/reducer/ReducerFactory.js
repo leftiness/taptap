03d0cd0f90694089c2b757f1630b2110e79261de
Brandon Parmenter <leftiness@gmail.com>
2017-03-11 20:50:38 -0500

    ReducerFactory updates
    - create() altered to accept init() as first arg, pure
    - Standard create() exported under alias withInit()
    - Alias used because function with first arg curried should be used
    - withInit() will be used in tests, don't test curried create()

--- src/redux/reducer/ReducerFactory.js
+++ src/redux/reducer/ReducerFactory.js
@@ -5,6 +5,7 @@ var Reducer = require('./Reducer.js')
 var State = require('./State.js')
 
 var IS_CURRIED = true
+var INIT = State.prototype.init
 
 /**
  * Provide a static Reducer create function
@@ -14,19 +15,26 @@ var ReducerFactory = t.struct(
   { name: 'ReducerFactory', strict: true }
 )
 
-var Create = t.func([ Reducer, t.maybe(State), Action ], State)
+var Init = State.prototype.Init
+
+var Create = t.func(
+  [ Init, Reducer, t.maybe(State), Action ],
+  State,
+  'Create'
+)
 
 /**
  * Create a Reducer factory which will eventually create a Reducer
  * function that won't have to worry about whether State is null
  */
-function create (reducer, state, action) {
-  if (!state) {
-    state = State.prototype.init()
-  }
-  return reducer(state, action)
+function create (init, reducer, state, action) {
+  return state
+    ? reducer(state, action)
+    : reducer(init(), action)
 }
 
-ReducerFactory.prototype.create = Create.of(create, IS_CURRIED)
+ReducerFactory.prototype.Create = Create
+ReducerFactory.prototype.withInit = Create.of(create, IS_CURRIED)
+ReducerFactory.prototype.create = ReducerFactory.prototype.withInit(INIT)
 
 module.exports = ReducerFactory
